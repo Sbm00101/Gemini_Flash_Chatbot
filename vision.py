450543dc82c090733118d502cbfe621acf4c09fe
Sbm00101 <137110211+Sbm00101@users.noreply.github.com>
2024-08-07 23:22:43 +0530

    Update vision.py

--- vision.py
+++ vision.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+
 load_dotenv()  # loading all env variables
 
 import streamlit as st
@@ -10,26 +11,33 @@ import PIL.Image
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 model = genai.GenerativeModel("gemini-1.5-flash")
 
+
 # Function to load Gemini model and get response
-def get_gemini_response(image, question):
-    img = PIL.Image.open(image)
-    response = model.generate_content([question, img], stream=True)
+def get_gemini_response(question, image=None):
+    if image:
+        img = PIL.Image.open(image)
+        response = model.generate_content([question, img], stream=True)
+    else:
+        response = model.generate_content([question], stream=True)
+
     response.resolve()
     return response.text
 
+
 # Function to handle the input and image submission
 def handle_submit():
-    if uploaded_image and st.session_state.input:
-        response = get_gemini_response(uploaded_image, st.session_state.input)
+    if st.session_state.input:
+        response = get_gemini_response(st.session_state.input, uploaded_image)
         st.session_state.response = response
     else:
-        st.session_state.response = "Please upload an image and enter a question."
+        st.session_state.response = "Please enter a question."
+
 
 # Streamlit frontend
 st.header("Gemini-SBM-Chat")
 
 # Image upload section
-uploaded_image = st.file_uploader("Upload an image:", type=["jpg", "jpeg", "png"])
+uploaded_image = st.file_uploader("Upload an image (optional):", type=["jpg", "jpeg", "png"])
 
 # Text input section with on_change callback
 st.text_input("Ask your question:", key="input", on_change=handle_submit)
